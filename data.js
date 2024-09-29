8aebfe78c8c2fc539979ee3de3d3e84b7789d00b
Sarah-Lisa Mero <sarahlisamero@gmail.com>
2024-09-29 16:27:11 +0200

    chore: add img

--- data.js
+++ data.js
@@ -22,67 +22,67 @@ const users = [
     {
       name: "Quintt Adam",
       linkedin: "https://www.linkedin.com/in/quintt-adam-138882224/",
-      image: "https://via.placeholder.com/150",
+      image: "./img/quinttadam_bas.jpg",
       role: "Strategic Designer"
     },
     {
       name: "Aeneas Desmet",
       linkedin: "https://www.linkedin.com/in/aeneas-desmet-a409111b8/",
-      image: "https://via.placeholder.com/150",
+      image: "./img/aeneasdesmet_bas.jpg",
       role: "Graphic Designer & Digital Creative"
     },
     {
       name: "Marie Coolens",
       linkedin: "https://www.linkedin.com/in/mariecoolens/",
-      image: "https://via.placeholder.com/150",
+      image: "./img/mariecoolens_bas.jpg",
       role: "Account"
     },
     {
       name: "Joke Van Bourgognie",
       linkedin: "https://www.linkedin.com/in/joke-van-bourgognie-713535209/",
-      image: "https://via.placeholder.com/150",
+      image: "./img/jokevanbourgognie_bas.jpg",
       role: "Account"
     },
     {
       name: "Floortje Bronda",
       linkedin: "https://www.linkedin.com/in/floortje-bronda-b70b751b7/",
-      image: "https://via.placeholder.com/150",
+      image: "./img/floortjebronda_bas.jpg",
       role: "Creative"
     },
     {
       name: "Mit Pynenborg",
       linkedin: "https://www.linkedin.com/in/mit-pynenborg?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app",
-      image: "https://via.placeholder.com/150",
+      image: "./img/mitpynenborg_bas.jpg",
       role: "Strategist/ Account"
     },
     {
       name: 'Josefine Tanghe',
       linkedin: "https://www.linkedin.com/in/josefinetanghe?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app",
-      image: "https://via.placeholder.com/150",
+      image: "./img/josefinetanghe_bas.jpg",
       role: "Strategist"
     },
     {
       name: "Elene Hoeben",
       linkedin: "www.linkedin.com/in/elene-hoeben-b43b94267",
-      image: "https://via.placeholder.com/150",
+      image: "./img/elenehoeben_bas.jpg",
       role: "Creative Director"
     },
     {
       name: "Raki Adu",
       linkedin: "https://be.linkedin.com/in/raki-a-b044a022a/nl",
-      image: "https://via.placeholder.com/150",
+      image: "./img/rakiadu_bas.jpg",
       role: "Creative"
     },
     {
       name: "Gitte Groven",
       linkedin:"https://www.linkedin.com/in/gitte-groven-3a0458260?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app",
-      image: "https://via.placeholder.com/150",
+      image: "./img/gittegroven_bas.jpg",
       role: "Creative Strategist",
     },
     {
       name: "Naomi Stynen",
       linkedin: "www.linkedin.com/in/naomistynen",
-      image: "https://via.placeholder.com/150",
+      image: "./img/naomistynen_bas.jpg",
       role: "Account"
     }
   ];

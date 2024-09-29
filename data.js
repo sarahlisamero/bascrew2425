32a553f0929d0bccadfcf05b3eac10d8d95d26f6
Sarah-Lisa Mero <sarahlisamero@gmail.com>
2024-09-29 16:13:45 +0200

    feat: add users

--- data.js
+++ data.js
@@ -4,14 +4,14 @@ const users = [
     {
       name: "Sarah-Lisa Mero",
       linkedin: "https://www.linkedin.com/in/sarah-lisa-m-130556190/",
-      image: "./img/sarahlisamero_bas.jpg", // Placeholder image link
-      role: "Creative strategist"
+      image: "./img/sarahlisamero_bas.jpg",
+      role: "Strategic Creative"
     },
     {
       name: "Giulia Calvo",
       linkedin: "https://www.linkedin.com/in/giulia-calvo/",
       image: "./img/giuliacalvo_bas.jpg",
-      role: "Project Manager"
+      role: "Account Manager"
     },
     {
       name: "Britt Adam",
@@ -19,5 +19,71 @@ const users = [
       image: "./img/brittadam_bas.jpg",
       role: "Digital Experience Designer"
     },
+    {
+      name: "Quintt Adam",
+      linkedin: "https://www.linkedin.com/in/quintt-adam-138882224/",
+      image: "https://via.placeholder.com/150",
+      role: "Strategic Designer"
+    },
+    {
+      name: "Aeneas Desmet",
+      linkedin: "https://www.linkedin.com/in/aeneas-desmet-a409111b8/",
+      image: "https://via.placeholder.com/150",
+      role: "Graphic Designer & Digital Creative"
+    },
+    {
+      name: "Marie Coolens",
+      linkedin: "https://www.linkedin.com/in/mariecoolens/",
+      image: "https://via.placeholder.com/150",
+      role: "Account"
+    },
+    {
+      name: "Joke Van Bourgognie",
+      linkedin: "https://www.linkedin.com/in/joke-van-bourgognie-713535209/",
+      image: "https://via.placeholder.com/150",
+      role: "Account"
+    },
+    {
+      name: "Floortje Bronda",
+      linkedin: "https://www.linkedin.com/in/floortje-bronda-b70b751b7/",
+      image: "https://via.placeholder.com/150",
+      role: "Creative"
+    },
+    {
+      name: "Mit Pynenborg",
+      linkedin: "https://www.linkedin.com/in/mit-pynenborg?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app",
+      image: "https://via.placeholder.com/150",
+      role: "Strategist/ Account"
+    },
+    {
+      name: 'Josefine Tanghe',
+      linkedin: "https://www.linkedin.com/in/josefinetanghe?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app",
+      image: "https://via.placeholder.com/150",
+      role: "Strategist"
+    },
+    {
+      name: "Elene Hoeben",
+      linkedin: "www.linkedin.com/in/elene-hoeben-b43b94267",
+      image: "https://via.placeholder.com/150",
+      role: "Creative Director"
+    },
+    {
+      name: "Raki Adu",
+      linkedin: "https://be.linkedin.com/in/raki-a-b044a022a/nl",
+      image: "https://via.placeholder.com/150",
+      role: "Creative"
+    },
+    {
+      name: "Gitte Groven",
+      linkedin:"https://www.linkedin.com/in/gitte-groven-3a0458260?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app",
+      image: "https://via.placeholder.com/150",
+      role: "Creative Strategist",
+    },
+    {
+      name: "Naomi Stynen",
+      linkedin: "www.linkedin.com/in/naomistynen",
+      image: "https://via.placeholder.com/150",
+      role: "Account"
+    }
   ];
   

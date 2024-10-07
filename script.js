f2c7aa7bf0a24b23b9b32e2357f534c6598b5a89
brittadam <britt.adam@hotmail.com>
2024-10-07 20:08:01 +0200

    Refactor: Update index.html and styles.css for team members page

--- script.js
+++ script.js
@@ -11,11 +11,11 @@ document.addEventListener("DOMContentLoaded", () => {
 
         // Insert user data into the card
         userCard.innerHTML = `
-            <img src="${user.image}" alt="${user.name}">
-            <h2>${user.name}</h2>
-            <p>${user.role}</p>
-            <a href="${user.linkedin}" target="_blank"><i class="fa-brands fa-linkedin"></i></a>
-        `;
+        <img src="${user.image}" alt="${user.name}">
+        <h2>${user.name}</h2>
+        <p>${user.role}</p>
+        <a href="${user.linkedin}" target="_blank"><i class="fa-brands fa-linkedin" style="color: #0A66C2;"></i></a>
+    `;    
 
         // Append the user card to the user list
         userList.appendChild(userCard);

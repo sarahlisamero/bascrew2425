624017aadbd13d3318dd52af75567da38128636f
Sarah-Lisa Mero <sarahlisamero@gmail.com>
2024-11-19 15:48:14 +0100

    feat: comment internships

--- data.js
+++ data.js
@@ -6,144 +6,168 @@ const users = [
       linkedin: "https://www.linkedin.com/in/sarah-lisa-m-130556190/",
       image: "./img/sarahlisamero_bas.jpg",
       role: "Digital Hybrid"
+      //Akqa
     },
     {
       name: "Giulia Calvo",
       linkedin: "https://www.linkedin.com/in/giulia-calvo/",
       image: "./img/giuliacalvo_bas.jpg",
       role: "Account Manager"
+      //Akqa
     },
     {
       name: "Britt Adam",
       linkedin: "https://www.linkedin.com/in/britt-adam-b7b682186/",
       image: "./img/brittadam_bas.jpg",
       role: "Digital Experience Designer"
+      //TBC
     },
     {
       name: "Quintt Adam",
       linkedin: "https://www.linkedin.com/in/quintt-adam-138882224/",
       image: "./img/quinttadam_bas.jpg",
       role: "Strategic Designer"
+      //Us
     },
     {
       name: "Aeneas Desmet",
       linkedin: "https://www.linkedin.com/in/aeneas-desmet-a409111b8/",
       image: "./img/aeneasdesmet_bas.jpg",
       role: "Graphic Designer & Digital Creative"
+      //Ogilvy Social.Lab
     },
     {
       name: "Marie Coolens",
       linkedin: "https://www.linkedin.com/in/mariecoolens/",
       image: "./img/mariecoolens_bas.jpg",
       role: "Account"
+      //LDV United
     },
     {
       name: "Joke Van Bourgognie",
       linkedin: "https://www.linkedin.com/in/joke-van-bourgognie-713535209/",
       image: "./img/jokevanbourgognie_bas.jpg",
       role: "Account"
+      //VML
     },
     {
       name: "Floortje Bronda",
       linkedin: "https://www.linkedin.com/in/floortje-bronda-b70b751b7/",
       image: "./img/floortjebronda_bas.jpg",
       role: "Creative"
+      //TBC
     },
     {
       name: "Mit Pynenborg",
       linkedin: "https://www.linkedin.com/in/mit-pynenborg?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app",
       image: "./img/mitpynenborg_bas.jpg",
       role: "Strategist"
+      //VML
     },
     {
       name: 'Josefine Tanghe',
       linkedin: "https://www.linkedin.com/in/josefinetanghe?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app",
       image: "./img/josefinetanghe_bas.jpg",
       role: "Strategist"
+      //LDV United
     },
     {
       name: "Elene Hoeben",
       linkedin: "www.linkedin.com/in/elene-hoeben-b43b94267",
       image: "./img/elenehoeben_bas.jpg",
       role: "Creative Director"
+      //TBC
     },
     {
       name: "Raki Adu",
       linkedin: "https://be.linkedin.com/in/raki-a-b044a022a/nl",
       image: "./img/rakiadu_bas.jpg",
       role: "Creative"
+      //TBC
     },
     {
       name: "Gitte Groven",
       linkedin:"https://www.linkedin.com/in/gitte-groven-3a0458260?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app",
       image: "./img/gittegroven_bas.jpg",
       role: "Creative Strategist",
+      //Akqa
     },
     {
       name: "Naomi Stynen",
       linkedin: "www.linkedin.com/in/naomistynen",
       image: "./img/naomistynen_bas.jpg",
       role: "Account"
+      //VML
     }, 
     {
       name: "Jef Mampaey",
       linkedin: "https://www.linkedin.com/in/jef-mampaey-828335252?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
       image: "./img/jefmampaey_bas.jpg",
       role: "Art Director"
+      //Happiness
     },
     {
       name: "Louise Ide",
       linkedin: "linkedin.com/in/louise-ide",
       image: "./img/louiseide_bas.jpg",
       role: "Account/ Project Manager"
+      //TBC
     },
     {
       name: "Marie De Meester",
       linkedin: "http://linkedin.com/in/marie-de-meester-84726423b",
       image: "./img/mariedemeester_bas.jpg",
       role: "Strategist and Creative"
+      //Berlin Creative Studio
     },
     {
       name: "Bjoke Claesen",
       linkedin: "https://www.linkedin.com/in/bjoke-claesen-70498b327?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app",
       image: "./img/bjokeclaesen_bas.jpg",
       role: "Social Media and Content Creator"
+      //TBC
     },
     {
       name: "Pieter Meeuwissen",
       linkedin: "https://www.linkedin.com/in/pieter-meeuwissen?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app",
       image: "./img/pietermeeuwissen_bas.jpg",
-      role: "Strategist"
+      role: "Art Director"
+      //VML
     },
     {
       name: "Lotte Steens",
       linkedin: "https://www.linkedin.com/in/lottesteens/",
       image: "./img/lottesteens_bas.jpg",
       role: "Strategist and Copywriter"
+      //Happiness
     },
     {
       name: "Emiel De Bleser",
       linkedin: "https://www.linkedin.com/in/emiel-de-bleser/",
       image: "./img/emieldebleser_bas.jpg",
       role: "Strategist"
+      //Mortier Brigade
     },
     {
       name: "Sofie Driesen",
       linkedin: "https://www.linkedin.com/in/sofie-driesen-394934228/",
       image:"./img/sofiedriesen_bas.jpg",
       role: "Account manager"
+      //TBC
     },
     {
       name: "Kenneth Dootselaere",
       linkedin: "https://www.linkedin.com/in/kenneth-dootselaere/",
       image: "./img/kennethdootselaere_bas.jpg",
       role: "Account manager"
+      //Ogilvy Social.Lab
     },
     {
       name: "Anke Sneijders",
       linkedin: "https://www.linkedin.com/in/anke-sneijders-6881b6258/",
       image: "./img/ankesneijders_bas.jpg",
       role: "Creative"
+      //TBC
     }
   ];
   
